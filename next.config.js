8eaf68629e868e9f0d17ade32e300d2c584d6065
wknowpark797 <wknowpark797@gmail.com>
2023-08-03 10:07:05 +0900

    next basic init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: false,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss";`,
+	},
+};
+
+module.exports = nextConfig;
