ec54f723a7646072eebc169c8c7bb7b23be8e2e4
wknowpark797 <wknowpark797@gmail.com>
2023-08-03 12:02:17 +0900

    Header.jsx 스타일 적용 (module.scss 사용)

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,30 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from './Header.module.scss';
 
 function Header() {
 	const router = useRouter();
 	const currentPath = router.pathname;
-	console.log('currentPath: ', currentPath);
 
 	return (
-		<header>
+		<header id={styles.header}>
 			<h1>
 				<Link href='/'>LOGO</Link>
 			</h1>
 
-			<ul id='gnb'>
+			<ul id={styles.gnb}>
 				<li>
-					<Link href='/ssg' className={currentPath === '/ssg' ? 'on' : ''}>
+					<Link href='/ssg' className={currentPath === '/ssg' ? styles.on : ''}>
 						SSG
 					</Link>
 				</li>
 				<li>
-					<Link href='/ssr' className={currentPath === '/ssr' ? 'on' : ''}>
+					<Link href='/ssr' className={currentPath === '/ssr' ? styles.on : ''}>
 						SSR
 					</Link>
 				</li>
 				<li>
-					<Link href='/isr' className={currentPath === '/isr' ? 'on' : ''}>
+					<Link href='/isr' className={currentPath === '/isr' ? styles.on : ''}>
 						ISR
 					</Link>
 				</li>
