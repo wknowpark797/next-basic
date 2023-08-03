62ee9082d1d29f37dccc3b073ecd2c187dd12e49
wknowpark797 <wknowpark797@gmail.com>
2023-08-03 11:14:49 +0900

    404 페이지 커스터마이징

---
+++ pages/404.jsx
@@ -0,0 +1,11 @@
+import SubLayout from '@/components/SubLayout';
+
+function notFound() {
+	return (
+		<SubLayout name={'404'}>
+			<h1>404 페이지 입니다.</h1>
+		</SubLayout>
+	);
+}
+
+export default notFound;
