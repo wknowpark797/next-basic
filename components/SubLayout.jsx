3d718702bccfb817a8f5c68a2c40cf9744f0166b
wknowpark797 <wknowpark797@gmail.com>
2023-08-04 09:52:13 +0900

    Next 내장 구글 폰트 적용

--- components/SubLayout.jsx
+++ components/SubLayout.jsx
@@ -1,6 +1,11 @@
 // 서브 페이지 전용 레이아웃
 import Head from 'next/head';
 import Header from './Header';
+import styles from './SubLayout.module.scss';
+import { Orbitron, Noto_Sans_KR } from 'next/font/google';
+
+const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
+const notoSans = Noto_Sans_KR({ subsets: [], weight: ['100', '300'], preload: true });
 
 function SubLayout(props) {
 	return (
@@ -11,8 +16,12 @@ function SubLayout(props) {
 
 			<section>
 				<Header />
-				<h1>{props.name}</h1>
-				{props.children}
+
+				<div className={styles.subLayout}>
+					<h1 className={orbitron.className}>{props.name}</h1>
+					<p className={notoSans.className}>레이아웃 컨텐츠 페이지입니다.</p>
+					{props.children}
+				</div>
 			</section>
 		</>
 	);
