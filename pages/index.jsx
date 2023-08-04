c3fedd1be117e63e518795334392fd029674b322
wknowpark797 <wknowpark797@gmail.com>
2023-08-04 11:11:19 +0900

    이미지 lazy loading 테스트

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,7 @@ export default function Home() {
 				<h1>Main</h1>
 
 				<div className={styles.pic}>
-					<Image src={pic} alt='pic' fill quality={50} placeholder='blur' />
+					<Image src={pic} alt='pic' fill quality={50} />
 				</div>
 
 				<div className={styles.pic}>
