72ff36ceca0357e3664e2c48c5b8e1771a12a8f6
wknowpark797 <wknowpark797@gmail.com>
2023-08-09 11:09:12 +0900

    react icons 설치 및 적용

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,9 @@ import Header from '@/components/Header';
 import Image from 'next/image';
 import pic from '@/public/img/pic.jpg';
 import { useEffect } from 'react';
+import { FaDev } from 'react-icons/fa6';
+import { FcServices } from 'react-icons/fc';
+import { IconContext } from 'react-icons';
 
 // api 라우팅: 서버요청 처리를 위해서는 express 프레임워크를 활용
 // -> next에서는 api폴더 안쪽에 서버쪽 요청 및 응답에 대한 라우팅 설정 가능
@@ -45,6 +48,19 @@ export default function Home() {
 			<main>
 				<Header />
 				<h1>Main</h1>
+
+				{/* 
+					IconContext.Provider 컴포넌트 import 후 웹폰트 아이콘을 활용한 부모요소에 wrapping 해주면 
+					해당 컴포넌트 안쪽에서는 context api를 이용해서 동일한 스타일을 전역으로 활용이 가능하다.
+				*/}
+				<IconContext.Provider value={{ size: '30px', color: 'blue', className: 'global-class-name' }}>
+					<FaDev />
+				</IconContext.Provider>
+
+				{/* 아이콘 개별 스타일 적용 */}
+				{/* 직접적으로 웹폰트 아이콘에 커스텀 클래스명, 사이즈, 컬러값 지정 가능 */}
+				<FaDev className='fontA' size='30px' color='red' />
+				<FcServices size='30px' />
 
 				<div className={styles.pic}>
 					<Image src={pic} alt='pic' fill quality={50} />
