ede8f9d789d490572b6c264709852ac7f4e05156
wknowpark797 <wknowpark797@gmail.com>
2023-08-09 09:57:18 +0900

    clsx 설치 및 구글 웹폰트 사용

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,30 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Header.module.scss';
+// nextjs 프레임워크에는 기본적으로 구글 웹폰트 시스템이 내장되어 있다.
+// next 자체적으로 구글 웹폰트의 용량을 최적화시켜 페이지에 적용
+// 비구조화할당으로 원하는 폰트 객체를 import
+import { Orbitron, Noto_Sans_KR } from 'next/font/google';
+
+// npm install clsx
+// 복수개의 클래스를 편하게 적용 (변수, 조건문)
+import clsx from 'clsx';
+
+// Orbitron 생성자로부터 사용할 객체를 반환 (subsets: 언어타입, weight: [사용할 폰트 굵기], preload: 사전로딩유무)
+// weight값이 등록되지 않으면 콘솔 오류
+// 반환된 폰트객체 안에는 클래스명이 프로퍼티로 등록되어 있다.
+// 해당 폰트를 사용하고자 하는 부모요소에 해당객체의 클래스명을 등록하면 자식컴포넌트에서는 자유롭게 해당 폰트 사용가능
+const orbitron = Orbitron({ subsets: ['latin'], weight: ['400', '500'], preload: true });
+const notoSans = Noto_Sans_KR({ subsets: ['latin'], weight: ['100', '300'], preload: true });
 
 function Header() {
 	const router = useRouter();
 	const currentPath = router.pathname;
 
 	return (
-		<header id={styles.header}>
+		// 하나의 엘리먼트 요소에 클래스명 복수 적용
+		// 복수개의 클래스명을 적용할 수 있지만 구글 웹폰트 클래스는 복수개의 클래스 적용이 불가능하다.
+		<header id={styles.header} className={clsx(orbitron.className, notoSans.className)}>
 			<h1>
 				<Link href='/'>LOGO</Link>
 			</h1>
