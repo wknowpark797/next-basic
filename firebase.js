219d1ed9a337662a0617eeed642190af2276c861
wknowpark797 <wknowpark797@gmail.com>
2023-08-11 11:07:07 +0900

    firebase 파일 추가 및 키정보 .env.local 저장

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API,
+	authDomain: 'dcode-community-11ff2.firebaseapp.com',
+	projectId: 'dcode-community-11ff2',
+	storageBucket: 'dcode-community-11ff2.appspot.com',
+	messagingSenderId: '1064287537669',
+	appId: '1:1064287537669:web:149e3b2716b18379f2aa6c',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
